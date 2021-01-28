9efe84649f858dc91c5389c79866b1e66a8dd944
jesse-online <clearyjd@gmail.com>
2021-01-28 13:46:43 -0500

    Add nav and Bootstrap source files

---
+++ bootstrap/js/index.esm.js
@@ -0,0 +1,35 @@
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap (v5.0.0-beta1): index.esm.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+import Collapse from '/bootstrap/js/src/collapse.js'
+
+// import Alert from '/bootstrap/js/src/alert'
+// import Button from '/src/button'
+// import Carousel from '/src/carousel'
+
+// import Dropdown from '/src/dropdown'
+// import Modal from '/src/modal'
+// import Popover from '/src/popover'
+// import ScrollSpy from '/src/scrollspy'
+// import Tab from '/src/tab'
+// import Toast from '/src/toast'
+// import Tooltip from '/src/tooltip'
+
+export {
+  Collapse,
+  
+  // Alert,
+  // Button,
+  // Carousel,
+  
+  // Dropdown,
+  // Modal,
+  // Popover,
+  // ScrollSpy,
+  // Tab,
+  // Toast,
+  // Tooltip
+}
