a02c87212217c80e2c3e57f73a791a2230752562
jesse-online <clearyjd@gmail.com>
2021-03-26 20:43:11 -0400

    Finalize styles

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
+// Animate main cat icon on hover over button
 const cat = document.querySelector('.fa-cat');
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('mouseover', () => {
    cat.classList.add('animate__animated', 'animate__bounce'); 
-    // console.log('moused');
 }, false);
+
