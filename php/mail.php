fabea637033d037c848e7c3610218d76e40ee71e
jesse-online <clearyjd@gmail.com>
2021-10-26 17:38:07 -0400

    Update php

--- php/mail.php
+++ php/mail.php
@@ -1,11 +1,14 @@
-<?php 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $formcontent="From: $name \n Message: $message";
-    $recipient = "jessecb@hey.com";
-    $subject = "Message from contact form";
-    $mailheader = "From: $email \r\n";
-    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-    echo "Thank you!";
+if($isset($_POST['submit']))
+{
+$name = $_POST['name'];
+$mailFrom = $_POST['email'];
+$message = $_POST['message']." August427";
+
+$mailTo = "jessecb@hey.com";
+$headers =  "From: ".$mailFrom;
+
+mail($mailTo, $name, $message, $headers);
+
+header("Location: index.html");
+}
 ?>
